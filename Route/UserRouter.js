d63e6464585911097bae800a8ecee2701b1685f8
EduardGhazaryan <137617257+EduardGhazaryan@users.noreply.github.com>
2025-02-07 12:02:54 +0400

    user get router add

--- Route/UserRouter.js
+++ Route/UserRouter.js
@@ -3,6 +3,8 @@ import UserController from "../Controller/UserController.js";
 
 const UserRouter = Router()
 
+UserRouter.get("/data", UserController.getUser)
+
 UserRouter.put("/change/:id", UserController.changeUser)
 
 UserRouter.delete("/remove/:id", UserController.removeUser)
